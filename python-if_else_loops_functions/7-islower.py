011196be26827a2bcb81f3d988974724f9a17db3
Abdiel-88 <abdiel.f@outlook.com>
2024-01-24 13:38:01 -0400

    task 7

--- python-if_else_loops_functions/7-islower.py
+++ python-if_else_loops_functions/7-islower.py
@@ -1,14 +1,9 @@
 def islower(c):
-    """Check if a character is lowercase."""
-    # ASCII value for 'a' is 97 and 'z' is 122
-    return 97 <= ord(c) <= 122
+    return ord('a') <= ord(c) <= ord('z')
 
-# The following lines are part of your test script and not the function itself
-if __name__ == "__main__":
-    islower = __import__('7-islower').islower
-
-    print("a is {}".format("lower" if islower("a") else "upper"))
-    print("H is {}".format("lower" if islower("H") else "upper"))
-    print("A is {}".format("lower" if islower("A") else "upper"))
-    print("3 is {}".format("lower" if islower("3") else "upper"))
-    print("g is {}".format("lower" if islower("g") else "upper"))
+# Test cases
+print("a is {}".format("lower" if islower("a") else "upper"))
+print("H is {}".format("lower" if islower("H") else "upper"))
+print("A is {}".format("lower" if islower("A") else "upper"))
+print("3 is {}".format("lower" if islower("3") else "upper"))
+print("g is {}".format("lower" if islower("g") else "upper"))
