444716dcae84cf66e4c19b11ece8c4a1482219d1
Abdiel-88 <abdiel.f@outlook.com>
2024-01-24 13:54:09 -0400

    task 8

---
+++ python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+def uppercase(str):
+    uppercase_str = ''
+    for char in str:
+        if 'a' <= char <= 'z':
+            uppercase_str += chr(ord(char) - 32)
+        else:
+            uppercase_str += char
+    print(uppercase_str)
+
+# Example usage
+uppercase("Hello World!")
