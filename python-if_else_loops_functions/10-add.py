4f0c9b4cf18dd94213890db9a41fdd9f47de41e4
Abdiel-88 <abdiel.f@outlook.com>
2024-01-24 14:03:40 -0400

    task 10

---
+++ python-if_else_loops_functions/10-add.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+def add(a, b):
+    return a + b
+
+# Example usage (these lines are for testing and should be in a separate file)
+# print(add(1, 2))
+# print(add(98, 0))
+# print(add(100, -2))
