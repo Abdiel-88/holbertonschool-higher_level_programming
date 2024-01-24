8dabae07b232c94dd8a5f7db4dc4fd4f6379e815
Abdiel-88 <abdiel.f@outlook.com>
2024-01-23 20:45:15 -0400

    task 2

--- python-if_else_loops_functions/2-print_alphabet.py
+++ python-if_else_loops_functions/2-print_alphabet.py
@@ -1,11 +1,4 @@
 #!/usr/bin/python3
 
-# Initialize an empty string
-alphabet = ''
-
-# Append each character to the string within the loop
 for i in range(97, 123):
-    alphabet += chr(i)
-
-# Use only one print function with string formatting
-print(f"{alphabet}", end='')
+    print(chr(i), end='')
