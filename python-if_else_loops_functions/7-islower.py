53d662246c2c7ece2bc34163a51cf1aceb6cc357
Abdiel-88 <abdiel.f@outlook.com>
2024-01-24 13:48:01 -0400

    task 7

--- python-if_else_loops_functions/7-islower.py
+++ python-if_else_loops_functions/7-islower.py
@@ -1,9 +1,7 @@
-def islower(c):
-    return ord('a') <= ord(c) <= ord('z')
+#!/usr/bin/python3
 
-# Test cases
-print("a is {}".format("lower" if islower("a") else "upper"))
-print("H is {}".format("lower" if islower("H") else "upper"))
-print("A is {}".format("lower" if islower("A") else "upper"))
-print("3 is {}".format("lower" if islower("3") else "upper"))
-print("g is {}".format("lower" if islower("g") else "upper"))
+def islower(c):
+    if 97 <= ord(c) <= 122:
+        return True
+    else:
+        return False
