d141b0c26d111b043826158990ccc655095fe567
Abdiel-88 <abdiel.f@outlook.com>
2024-01-24 14:08:34 -0400

    task 11

---
+++ python-if_else_loops_functions/11-pow.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+def pow(a, b):
+    return a ** b
+
+# Example usage (these lines are for testing and should be in a separate file)
+# print(pow(2, 2))
+# print(pow(98, 2))
+# print(pow(98, 0))
+# print(pow(100, -2))
+# print(pow(-4, 5))
