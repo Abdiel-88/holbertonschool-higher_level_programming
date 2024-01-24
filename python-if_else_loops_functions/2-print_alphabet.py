a9ef86f4c6d2c4e4673318e45b276f7dae403105
Abdiel-88 <abdiel.f@outlook.com>
2024-01-23 20:50:11 -0400

    task 2

--- python-if_else_loops_functions/2-print_alphabet.py
+++ python-if_else_loops_functions/2-print_alphabet.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
 
-# Looping from 97 to 123, which are the ASCII values for 'a' to 'z'
 for i in range(97, 123):
-    # Using string formatting to print each character directly
     print("{}".format(chr(i)), end='')
