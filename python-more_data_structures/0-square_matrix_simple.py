6530ee3b77992e85b3f7b44a7d8929faf3a18a14
Abdiel-88 <abdiel.f@outlook.com>
2024-01-30 00:03:28 -0400

    execute

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    return [[element ** 2 for element in row] for row in matrix]
+    if matrix:
+        return [[element ** 2 for element in row] for row in matrix]
+    return []
 
 if __name__ == "__main__":
     matrix = [
