b60590508a91e79c0f041426f4d8cd1e4bf5426e
Abdiel-88 <abdiel.f@outlook.com>
2024-02-01 17:23:01 -0400

    task 4

---
+++ python-classes/4-square.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+"""
+Defines a class Square with a private instance attribute size,
+with property and property setter.
+"""
+
+
+class Square:
+    """
+    Defines a square by its size with validation,
+    and calculates its area.
+    """
+
+    def __init__(self, size=0):
+        self.size = size
+
+    @property
+    def size(self):
+        """Retrieves the size of the square."""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Sets the size of the square, with validation."""
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """Returns the current square area."""
+        return self.__size ** 2
+
+
+if __name__ == "__main__":
+    Square = __import__('4-square').Square
+
+    my_square = Square(89)
+    print("Area: {} for size: {}".format(my_square.area(), my_square.size))
+
+    my_square.size = 3
+    print("Area: {} for size: {}".format(my_square.area(), my_square.size))
+
+    try:
+        my_square.size = "5 feet"
+        print("Area: {} for size: {}".format(my_square.area(), my_square.size))
+    except Exception as e:
+        print(e)
