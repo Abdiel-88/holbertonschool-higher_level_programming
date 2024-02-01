8f42110b3efb9eab1e664aae9ced149b4cbe4116
Abdiel-88 <abdiel.f@outlook.com>
2024-02-01 14:49:57 -0400

    task 0, square class

---
+++ python-classes/0-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Defines a class Square that represents a square."""
+
+
+class Square:
+    """An empty class that defines a square."""
+    pass
+
+
+# This block is for testing and will not be executed when imported.
+if __name__ == "__main__":
+    my_square = Square()
+    print(type(my_square))
+    print(my_square.__dict__)
