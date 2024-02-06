fa51e7e226305382a3592a0a7e876c23a253dabc
Abdiel-88 <abdiel.f@outlook.com>
2024-02-06 00:20:41 -0400

    task 3 again

--- python-more_classes/3-rectangle.py
+++ python-more_classes/3-rectangle.py
@@ -3,6 +3,7 @@
 This module defines a Rectangle class with width and height attributes.
 """
 
+
 class Rectangle:
     """Defines a rectangle with width and height attributes."""
 
@@ -51,11 +52,17 @@ class Rectangle:
         return 2 * (self.__width + self.__height)
 
     def __str__(self):
-        """Returns a string representation of the rectangle using '#' characters."""
+        """
+        Returns a string representation of the
+        rectangle using '#' characters.
+        """
         if self.__width == 0 or self.__height == 0:
             return ""
         return "\n".join(['#' * self.__width] * self.__height)
 
     def __repr__(self):
         """Returns a string representation of the rectangle object."""
-        return "<{}.{}, {}>".format(self.__class__.__module__, self.__class__.__name__, self.__dict__)
+        class_name = self.__class__.__name__
+        module_name = self.__class__.__module__
+        attributes = str(self.__dict__)
+        return f"<{module_name}.{class_name}, {attributes}>"
