c58af2c21b57f174e8f0e15dfcebee93509d305c
Abdiel-88 <abdiel.f@outlook.com>
2024-02-07 19:01:06 -0400

    fix pycodestyle

--- python-inheritance/3-is_kind_of_class.py
+++ python-inheritance/3-is_kind_of_class.py
@@ -5,6 +5,7 @@ if an object is an instance of, or if the object is an instance of a class
 that inherited from, the specified class.
 """
 
+
 def is_kind_of_class(obj, a_class):
     """Check if an object is an instance of, or if it is an instance of a
     class that inherited from, the specified class.
