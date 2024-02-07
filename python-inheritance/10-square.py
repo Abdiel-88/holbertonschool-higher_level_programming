77b6ee648519f117afc998f99b7783d77b0f02cf
Abdiel-88 <abdiel.f@outlook.com>
2024-02-07 19:47:01 -0400

    fix pycodestyle

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -1,11 +1,17 @@
 #!/usr/bin/python3
-"""
-This module defines the Square class that inherits from Rectangle.
-"""
+"""Defines a Rectangle subclass Square."""
+Rectangle = __import__('9-rectangle').Rectangle
 
-Square = __import__('10-square').Square
 
-s = Square(13)
+class Square(Rectangle):
+    """Represent a square."""
 
-print(s)  # Uses the __str__ method
-print(s.area())  # Calls the area method
+    def __init__(self, size):
+        """Initialize a new square.
+
+        Args:
+            size (int): The size of the new square.
+        """
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
