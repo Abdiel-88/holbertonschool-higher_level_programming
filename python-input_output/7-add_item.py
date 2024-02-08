5acb6218cc751ba549c1e1ac2aa6243de061b07f
Abdiel-88 <abdiel.f@outlook.com>
2024-02-08 14:10:32 -0400

    task 7

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,29 +1,18 @@
 #!/usr/bin/python3
-"""
-Module to add all command-line arguments to a list
-and save them to 'add_item.json'.
-Utilizes functions from 'save_to_json_file'
-and 'load_from_json_file' modules.
-"""
+
+"""Add all arguments to a Python list and save them to a file."""
 
 
 import sys
 
-# Assuming these functions are in the same directory as the script
-from save_to_json_file import save_to_json_file
-from load_from_json_file import load_from_json_file
+if __name__ == "__main__":
+    save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
+    load_from_json_file = \
+        __import__('8-load_from_json_file').load_from_json_file
 
-filename = "add_item.json"
-
-try:
-    # Attempt to load existing items from the file
-    items = load_from_json_file(filename)
-except FileNotFoundError:
-    # Initialize items list if the file doesn't exist
-    items = []
-
-# Add command-line arguments to the list, skipping the script name
-items.extend(sys.argv[1:])
-
-# Save the updated items list back to the file
-save_to_json_file(items, filename)
+    try:
+        items = load_from_json_file("add_item.json")
+    except FileNotFoundError:
+        items = []
+    items.extend(sys.argv[1:])
+    save_to_json_file(items, "add_item.json")
