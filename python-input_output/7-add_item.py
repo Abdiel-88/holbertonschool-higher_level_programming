303629911fcc722bd0edc0bebf23cfdd6467a722
Abdiel-88 <abdiel.f@outlook.com>
2024-02-08 14:04:05 -0400

    task 7

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -7,8 +7,8 @@ The list is saved as a JSON representation in a file named add_item.json.
 import sys
 
 # Assuming these functions are in the same directory as the script
-from 5-save_to_json_file import save_to_json_file
-from 6-load_from_json_file import load_from_json_file
+from save_to_json_file import save_to_json_file
+from load_from_json_file import load_from_json_file
 
 filename = "add_item.json"
 
