387bcadfea46da1b5904f7658bdf3f1bb6213589
Abdiel-88 <abdiel.f@outlook.com>
2024-02-08 13:36:51 -0400

    fix pycodestyle

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -6,7 +6,10 @@ and returns the number of characters written.
 
 
 def write_file(filename="", text=""):
-    """Write a string to a text file (UTF-8) and return the number of characters written."""
+    """
+    Write a string to a text file (UTF-8)
+    and return the number of characters written.
+    """
     with open(filename, mode="w", encoding="utf-8") as f:
         return f.write(text)
 
