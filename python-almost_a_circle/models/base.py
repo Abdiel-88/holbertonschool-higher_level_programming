e97d3c6a86ad098e6a58f66dd8baa48e4eb81e7d
Abdiel-88 <abdiel.f@outlook.com>
2024-02-12 18:46:24 -0400

    base again

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -1,24 +1,29 @@
 #!/usr/bin/python3
-"""Defines a base model class."""
-import json
-import csv
-import turtle
-
 """
-This module defines the Base class.
-The Base class serves as the foundation for all other classes in this project.
-It manages an id attribute for instances to avoid duplicating code.
+This module defines the Base class for the project. It aims to manage id
+attributes in all future classes and avoid duplicating code.
 """
 
 
 class Base:
+    """
+    The Base class to act as the foundation for all other classes in this
+    project.
+
+    Attributes:
+        __nb_objects (int): A class attribute to count the number of objects
+                            if no id is given.
+    """
+
     __nb_objects = 0
 
     def __init__(self, id=None):
-        """Initialize a new Base instance.
+        """
+        Initializes a new Base instance.
+
         Args:
-            id (int): The id of the instance.
-            If None, an id is automatically assigned.
+            id (Optional[int]): The id of the instance. If None, an id is
+                                automatically assigned.
         """
         if id is not None:
             self.id = id
