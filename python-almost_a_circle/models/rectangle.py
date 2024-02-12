6d4f2c23bf027a5ff283fd8d4fa4bf37650c7ac5
Abdiel-88 <abdiel.f@outlook.com>
2024-02-12 19:33:40 -0400

    rectangle task 2

---
+++ python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+from models.base import Base
+
+
+class Rectangle(Base):
+    def __init__(self, width, height, x=0, y=0, id=None):
+        super().__init__(id)
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value <= 0:
+            raise ValueError("width must be > 0")
+        self.__width = value
+
+    # Implement similar getter and setter for height, x, and y
