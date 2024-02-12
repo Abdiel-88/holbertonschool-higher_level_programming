0ea4a33ee9cbbd8109ae31608b4c7a5491623756
Abdiel-88 <abdiel.f@outlook.com>
2024-02-12 18:18:35 -0400

    task 0

---
+++ python-almost_a_circle/models/base.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+This module defines the Base class.
+The Base class serves as the foundation for all other classes in this project.
+It manages an id attribute for instances to avoid duplicating code.
+"""
+
+
+class Base:
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """Initialize a new Base instance.
+        Args:
+            id (int): The id of the instance.
+            If None, an id is automatically assigned.
+        """
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
