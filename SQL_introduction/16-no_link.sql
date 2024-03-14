07a4100e8bef6495467a150c691dca8d8ab9b191
Abdiel-88 <abdiel.f@outlook.com>
2024-03-14 16:50:23 -0400

    task 16

---
+++ SQL_introduction/16-no_link.sql
@@ -0,0 +1,2 @@
+-- List all records with a non-empty name from second_table, ordered by descending score
+SELECT score, name FROM second_table WHERE name IS NOT NULL AND name <> '' ORDER BY score DESC;
