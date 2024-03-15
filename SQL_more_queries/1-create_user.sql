85d3670b2a3f8c8f51cafb176b794a289093a2f3
Abdiel-88 <abdiel.f@outlook.com>
2024-03-15 13:53:00 -0400

    task 1

---
+++ SQL_more_queries/1-create_user.sql
@@ -0,0 +1,8 @@
+-- Attempt to create user_0d_1 if they don't already exist
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+
+-- Grant all privileges to user_0d_1 on all databases and tables
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
+
+-- Apply the changes immediately
+FLUSH PRIVILEGES;
