99af4a9ec992b1bbd0b1e95d5a4592a29f541fed
Abdiel-88 <abdiel.f@outlook.com>
2024-03-15 15:02:46 -0400

    task 13,14,15,16

---
+++ SQL_more_queries/14-my_genres.sql
@@ -0,0 +1,7 @@
+-- List all genres of the show Dexter from hbtn_0d_tvshows database. Sort by genre name in ascending order.
+SELECT genres.name
+FROM tv_shows
+JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+JOIN genres ON tv_show_genres.genre_id = genres.id
+WHERE tv_shows.title = 'Dexter'
+ORDER BY genres.name ASC;
