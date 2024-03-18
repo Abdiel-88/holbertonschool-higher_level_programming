4cc5c14ffe02f79021935d7b7672f80e3a306259
Abdiel-88 <abdiel.f@outlook.com>
2024-03-18 15:00:35 -0400

    task 1 executable

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -1,17 +1,55 @@
 #!/usr/bin/python3
-"""  lists all states from database hbtn_0e_0_usa """
+"""
+A script that lists all states with a name starting with N
+from the database hbtn_0e_0_usa.
+This script takes 3 arguments: mysql username, mysql password, and
+database name. It uses the MySQLdb module to connect to a MySQL server
+running on localhost at port 3306. Results are sorted in ascending order
+by states.id and displayed as specified.
+"""
+
 import MySQLdb
 import sys
 
 
+def connect_to_database(username, password, db_name):
+    """
+    Connects to the MySQL database with the provided credentials.
+    """
+    try:
+        conn = MySQLdb.connect(host="localhost", port=3306, user=username,
+                               passwd=password, db=db_name, charset="utf8")
+        return conn
+    except MySQLdb.Error as err:
+        print(f"Cannot connect to database: {err}")
+        sys.exit(1)
+
+
+def list_states(conn):
+    """
+    Lists all states from the database with names starting with 'N',
+    in ascending order by their ID.
+    """
+    cur = conn.cursor()
+    try:
+        cur.execute("SELECT * FROM st WHERE name LIKE 'N%' ORDER BY id ASC")
+        query_rows = cur.fetchall()
+        for row in query_rows:
+            print(row)
+    except MySQLdb.Error as err:
+        print(f"SQL Error: {err}")
+    finally:
+        cur.close()
+
+
 if __name__ == "__main__":
-    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
-                         passwd=sys.argv[2], db=sys.argv[3], port=3306)
-    cur = db.cursor()
-    cur.execute("""SELECT * FROM states WHERE name
-                LIKE BINARY 'N%' ORDER BY states.id""")
-    rows = cur.fetchall()
-    for row in rows:
-        print(row)
-    cur.close()
-    db.close()
+    if len(sys.argv) != 4:
+        print("Usage: ./1-filter_states.py <mysql username> <mysql password>"
+              " <database name>")
+        sys.exit(1)
+
+    mysql_username, mysql_password, database_name = sys.argv[1],
+    sys.argv[2], sys.argv[3]
+    conn = connect_to_database(mysql_username, mysql_password, database_name)
+    list_states(conn)
+    conn.close()
